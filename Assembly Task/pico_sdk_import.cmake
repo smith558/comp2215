1d1d62e664e125d42f8974c5fb4585ef91ab9e24
Stanislav Modrak <stanislav.modrak@gmail.com>
2022-05-10 19:36:05 +0100

    Update code

--- Assembly Task/pico_sdk_import.cmake
+++ Assembly Task/pico_sdk_import.cmake
@@ -3,6 +3,9 @@
 # This can be dropped into an external project to help locate this SDK
 # It should be include()ed prior to project()
 
+# setting using git as default
+SET(PICO_SDK_FETCH_FROM_GIT 1)
+
 if (DEFINED ENV{PICO_SDK_PATH} AND (NOT PICO_SDK_PATH))
     set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
     message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
